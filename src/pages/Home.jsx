96bd5d612da4c400ddf4d3796281c1d2db1f6c2e
rjsgud49 <qkrrjsgud49@gmail.com>
2024-12-18 17:01:37 +0900

    dmdl

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
+
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
+
 const Home = () => {
-  return <div>Home</div>;
+  const[pivotDate,setPivotdate]=useState(new Date());
+  return (
+    <div>
+      <Header
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button text={"<"} />}
+        rightChild={<Button text={">"} />}
+      />
+      <DiaryList />
+    </div>
+  );
 };
 
 export default Home;
